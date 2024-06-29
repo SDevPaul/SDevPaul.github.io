8891a1a131d8dcedf16768c317d264e59a45e7b2
SDevPaul <paulsevillejo05@gmail.com>
2024-06-30 01:10:50 +0400

    Delete Portfolio directory

--- Portfolio/jscript/button.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.btns').forEach(button => {
-    button.addEventListener('click', function() {
-        const url = this.getAttribute('data-url');
-        window.location.href = url;
-    });
-});
